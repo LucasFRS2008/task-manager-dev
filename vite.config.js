33e292233ccdb3cdf9330f0df8ff6f01b285812b
Lucas Ferreira Rodrigues Silva <lucasrodrigues@MacBook-Air-de-Lucas.local>
2026-09-08 16:54:29 -0300

    Finaliza o projeto

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/task-manager-dev/',
+  base: './', // <-- Isso garante que os caminhos dos arquivos funcionem corretamente
 })
